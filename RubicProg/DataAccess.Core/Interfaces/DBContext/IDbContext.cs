2f3a8a462ca38b357bc1788516b7200d92e9bfb7
SolomonidinG <danil-egin@mail.ru>
2022-03-05 15:31:56 +0300

    Add files

---
+++ RubicProg/DataAccess.Core/Interfaces/DBContext/IDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RubicProg.DataAccess.Core.Interfaces.DBContext
+{
+    public interface IDbContext
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ RubicProg/DataAccess/Context/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RubicProg.DataAccess.Context
+{
+    public class DataBaseContext
+    {
+        public int Id { get; set; }
+    }
+}
